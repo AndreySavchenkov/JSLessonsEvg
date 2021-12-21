df0dcd39e870e5d6c11eeff56e031e6a9c5bf60f
sandvik16 <sannndvik16@gmail.com>
2021-12-21 12:55:07 +0300

    this, apply/bind/call

--- src/lessons/lesson5/lesson5.ts
+++ src/lessons/lesson5/lesson5.ts
@@ -20,6 +20,28 @@ console.log('Lesson 5');
 // https://learn.javascript.ru/call-apply-decorators
 // https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%BE-%D0%BE-%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%B0%D1%85-apply-call-%D0%B8-bind-%D0%BD%D0%B5%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B8%D0%BC%D1%8B%D1%85-%D0%BA%D0%B0%D0%B6%D0%B4%D0%BE%D0%BC%D1%83-javascript-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D1%83-ddd5f9b06290
 
+//Function's methods
+///Bind
+
+// let obj = {
+//     name: 'Evgen',
+//     testFunc() {
+//         console.log('testFunc', this)
+//     }
+// };
+// setTimeout(obj.testFunc, 0)
+
+// let obj2 = {
+//     name: 'Hanna'
+// }
+//
+// let bindFunction = obj.testFunc.bind(obj2)
+// bindFunction()
+
+
+
+
+
 
 // Task 01
 // Дан объект someObj, реализуйте функцию greeting и присвойте ее ключу объекта с аналогичным именем.
